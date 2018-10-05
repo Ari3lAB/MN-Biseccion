bf63a714cf3fb7da2249ef8f6aec67ca2c165747
Ari3lAB <36025299+Ari3lAB@users.noreply.github.com>
2018-10-04 19:40:30 -0700

    Looped

--- src/biseccion/Biseccionador.java
+++ src/biseccion/Biseccionador.java
@@ -1,77 +1,80 @@
 package biseccion;
+
 import java.util.Scanner;
+
 /**
  *
  * @author Ariel AB
  */
 public class Biseccionador {
-    
-    private float limIzq = 0;
-    private float limDer = 0;
-    private float error = 0;
+
     Scanner teclado = new Scanner(System.in);
-    public void bisecciona(float limIzq, float limDer, float error){
-        float fxI=f(limIzq);
-        float fxD=f(limDer);
-        float xM=xMedia(limIzq,limDer);
+
+    public void bisecciona(double limIzq, double limDer, double error) {
+        double xI = limIzq;
+        double xD = limDer;
+        double eMax = error;
+        double fxI = f(xI);
+        double fxD = f(xD);
+        double xM = xMedia(xI, xD);
+        double fxM = f(xM);
         short contador = 1;
-        boolean izqNeg = true;
-        if (limIzq >= limDer || limIzq == limDer) {
+
+        if (xI >= xD || xI == xD) {
             System.out.println("Valores de entrada invalidos");
             return;
         }
-        
-        if (fxI>0) {
-            izqNeg= false;
-        }
-        
-        if( (fxI>0 && fxD>0) && (fxI<0 && fxD<0)){
-            System.out.println("No hay raices en el itervalo");
-            return;
-        }else{
-            System.out.println("Valores iniciales: X izq= " + limIzq + "  X der= " + limDer + "Error: " + error);
-        }        
-        while(error > Math.abs(f(xM))|| Math.abs(f(xM))==0 ){
+
+        System.out.println("Valores iniciales: X izq= " + xI + "  X der= " + xD + " Error: " + eMax);
+
+        while (Math.abs(fxM) >= eMax) {
             System.out.println("\nIteracion #" + contador);
             System.out.println("X media= " + xM);
             System.out.println("F(X) izquierda= " + fxI);
             System.out.println("F(X) derecha= " + fxD);
-            System.out.println("F(X) media= " + f(xM));
-            if(f(xM) < 0 && izqNeg == true){
-                limIzq=xM;
-            } else{
-                limDer=xM;
+            System.out.println("F(X) media= " + fxM);
+            if (fxM * fxI > 0) {
+                xI = xM;
+                fxI = f(xI);
+                fxD = f(xD);
+                xM = xMedia(xI, xD);
+                fxM = f(xM);
+            } else {
+                xD = xM;
+                fxI = f(xI);
+                fxD = f(xD);
+                xM = xMedia(xI, xD);
+                fxM = f(xM);
             }
-            
-            xM=xMedia(limIzq,limDer);
-            fxI=f(limIzq);
-            fxD=f(limDer);            
+
             contador++;
-            
-            System.out.println("Nueva X izq= " + limIzq);
-            System.out.println("Nueva X der= " + limDer);
+            System.out.println("Nueva X izq= " + xI);
+            System.out.println("Nueva X der= " + xD);
         }
-        
         System.out.println("Raiz: " + xM);
-        
+
     }
-    public float f(float x){
-        float fx;
-        fx= (float) (((-12 * (Math.pow(x, 5))) - (6.4 * (Math.pow(x, 3))) + 12));
+
+    public double f(double x) {
+        double fx;
+        fx = (double) (-12 * (Math.pow(x, 5))) - (6.4 * (Math.pow(x, 3))) + 12;
         return fx;
     }
-    
-    public float xMedia(float x1, float x2){
+
+    public double xMedia(double x1, double x2) {
         return ((x1 + x2) / 2);
     }
-    
-    public void main(){
+
+    public void main() {
+        double limIzq;
+        double limDer;
+        double error;
         System.out.println("Ingrese el limite izquierdo:");
-        this.limIzq = teclado.nextFloat();
+        limIzq = teclado.nextDouble();
         System.out.println("Ingrese el limite derecho:");
-        this.limDer = teclado.nextFloat();
+        limDer = teclado.nextDouble();
         System.out.println("Ingrese el error máximo:");
-        this.error = teclado.nextFloat();
+        error = teclado.nextDouble();
         bisecciona(limIzq, limDer, error);
     }
 }
